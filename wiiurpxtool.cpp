294f608ca93860fa71d1fd7b132d3752c269c19b
0CBH0 <maodatou88@163.com>
2017-06-25 22:40:39 +0800

    fix the wrong offset of CRC data, close #2

--- wiiurpxtool.cpp
+++ wiiurpxtool.cpp
@@ -173,6 +173,7 @@ int decompress(FILE *in, FILE *out)
 		fwrite32_BE(shdr_table[i].sh_addralign, out);
 		fwrite32_BE(shdr_table[i].sh_entsize, out);
 	}
+	
 	fseek(out, crc_data_offset, 0);
 	for (u32 i=0; i<ehdr.e_shnum; i++)
 		fwrite32_BE(crcs[i], out);
@@ -365,6 +366,7 @@ int compress(FILE *in, FILE *out)
 		fwrite32_BE(shdr_table[i].sh_addralign, out);
 		fwrite32_BE(shdr_table[i].sh_entsize, out);
 	}
+
 	fseek(out, crc_data_offset, 0);
 	for (u32 i=0; i<ehdr.e_shnum; i++)
 		fwrite32_BE(crcs[i], out);
@@ -378,7 +380,7 @@ int main(int argc, char *argv[])
 {
 	if(argc != 3)
 	{
-		printf("wiiurpxtool - version:1.1\n");
+		printf("wiiurpxtool - version:1.2\n");
 		printf("Compress or decompress RPL/RPX files for Wii U\n\n");
 		printf("Usage:\n");
 		printf("decompress:\n");
@@ -403,9 +405,9 @@ int main(int argc, char *argv[])
 	}
 	fclose(in);
 	fclose(out);
-	//if(result == 0)
-	//	fcopy("temp.bin", argv[2]);
-	//remove("temp.bin");
+	if(result == 0)
+		fcopy("temp.bin", argv[2]);
+	remove("temp.bin");
 	return 0;
 }
 
